ca48066d33c239310a1061c4f77edc7a37f81c1b
Tomokazu Tantaka <t.tantaka@eukarya.io>
2026-05-16 15:44:05 +0900

    xxx

--- 3DPartsModel/test3.py
+++ 3DPartsModel/test3.py
@@ -15,86 +15,137 @@ import base
 # 初期化
 base.init()
 
-batteryD = 57*2
+INCH = 6 * 25.4  # 6inch
 
-Thickness = 1.75
+MOTOR_X = INCH / 2 * 1.47
 
-# body ----------------------------------------
+
+# arm ----------------------------------------
+
+arm_width = 14.5
+arm_height = arm_width*3
+arm_power = 0.75
+
+def create_arm():
+    arm = base.create_tear_beam(
+        depth=MOTOR_X,  # ビームの長さ（Y軸）
+        width=arm_width,  # 断面の幅（X）
+        height=arm_height,  # 断面の涙型の高さ（Z）
+        power=arm_power,
+    )
+    inner = base.create_tear_beam(
+        depth=MOTOR_X*1.5,  # ビームの長さ（Y軸）
+        width=arm_width *0.85,  # 断面の幅（X）
+        height=arm_height *0.85,  # 断面の涙型の高さ（Z）
+        power=arm_power,
+    )
+    base.modifier_apply(inner, arm, operation="DIFFERENCE")
+    base.cut_cube(
+        target=arm,
+        scale=(12.0, INCH, 5.0),
+        location=(0, 0, -3.9),
+    )
+    return arm
+
+### motor ----------------------------------------
+
+def create_dummy():
+    motor_dummy = base.create_cylinder(radius=38.2 / 2, depth=30.7)
+    prop_dummy = base.create_cylinder(radius=INCH / 2, depth=9.0, location=(0, 0, (30.7 + 9.0) / 2))
+    base.modifier_apply(obj=prop_dummy, target=motor_dummy, operation="UNION")
+    return motor_dummy
+
+def create_motor():
+
+    R = 38.2 / 2
+    D = R * 6
+
+    motor = base.create_cylinder(radius=R, depth=D)
+    base.taper(motor, segments=64, curve="tear", power=0.75)
+    
+    # arm attach --------------------------------
+    
+    arm = create_arm()
+    arm.location = (0, MOTOR_X / 2, -6.085)
+    base.modifier_apply(obj=arm, target=motor, operation="UNION")
+    
+    # 内側をくり抜く（壁厚1.5mm）
+    inner = base.create_cylinder(radius=R*0.97, depth=D*0.97)
+    base.taper(inner, segments=64, curve="tear", power=0.75)
+    base.modifier_apply(inner, motor, operation="DIFFERENCE")
+    
+    base.cut_cylinder(
+        target=motor,
+        radius=R,
+        depth=100.0,
+        location=(0, 0, 50*0.75),
+    )
+    
+    # 中身を確認するために透過
+    base.cut_cube(
+        target=motor,
+        scale=(50, 50, 250),
+        location=(0, -25, 0),
+    )
+
+    # dummy --------------------------------
+#    dummy = create_dummy()
+#    dummy.location = (0, 0, 3.4)
+#    base.modifier_apply(obj=dummy, target=motor, operation="UNION")
+
+    return motor
+
+
+
+motor1 = create_motor()
+motor1.location = (0, -MOTOR_X, 0)
+
+#motor2 = create_motor()
+#motor2.location = (MOTOR_X, 0, 0)
+#motor2.rotation_euler[2] = math.pi / 2
+
+#motor3 = create_motor()
+#motor3.location = (-MOTOR_X, 0, 0)
+#motor3.rotation_euler[2] = -math.pi / 2
+
+#motor4 = create_motor()
+#motor4.location = (0, MOTOR_X, 0)
+#motor4.rotation_euler[2] = math.pi
+
+## body ----------------------------------------
 
 # 涙型の流線型ボディ
-R = 28.5
-D = R*6
-body = base.create_cylinder(
-    radius=R,
-    depth=D,
-)
-base.taper(body, segments=64, curve="tear", power=0.75)
+R = 33.0
+D = R * 10
+
+body = base.create_cylinder(radius=R, depth=D)
+base.taper(body, segments=64, curve="tear", power=0.60)
+
+## arm attach
+#base.modifier_apply(obj=arm1, target=body, operation="UNION")
+##base.modifier_apply(obj=arm2, target=body, operation="UNION")
+##base.modifier_apply(obj=arm3, target=body, operation="UNION")
+##base.modifier_apply(obj=arm4, target=body, operation="UNION")
 
 # 内側をくり抜く（壁厚1.5mm）
-inner = base.create_cylinder(
-    radius=R-Thickness,
-    depth=D,
-)
-base.taper(inner, segments=16, curve="tear", power=0.75)
+inner = base.create_cylinder(radius=R*0.97, depth=D*0.97)
+base.taper(inner, segments=64, curve="tear", power=0.60)
 base.modifier_apply(inner, body, operation="DIFFERENCE")
 
-base.cut_cylinder(
+# 無駄な底辺をカット
+base.cut_cube(
     target=body,
-    radius=R,
-    depth=120.0,
-    location = (0, 0, 41.0)
+    scale=(100, 100, 500),
+    location=(0, 0, 320),
 )
 
-body.location = (0, 0, -batteryD /2+19)
 
-## battery ----------------------------------------
+### ----------------------------------------
 
-#R = 28.5
-battery = base.create_cylinder(
-    radius=R,
-    depth=batteryD,
+# 中身を確認するために透過
+base.cut_cube(
+    target=body,
+    scale=(80, 40, 500),
+    location=(0, -20, 0),
 )
 
-base.cut_cylinder(
-    target=battery,
-    radius=R-Thickness,
-    depth=batteryD,
-)
-
-base.modifier_apply(obj=battery, target=body, operation="UNION")
-
-## motor ----------------------------------------
-
-#R = 37.5/2
-#D = R*6
-#motor = base.create_cylinder(
-#    radius=R,
-#    depth=D,
-#)
-#base.taper(motor, segments=64, curve="tear", power=0.75)
-
-## 内側をくり抜く（壁厚1.5mm）
-##inner = base.create_cylinder(
-##    radius=R-1.5,
-##    depth=D,
-##)
-##base.taper(inner, segments=16, curve="tear", power=0.75)
-##base.modifier_apply(inner, motor, operation="DIFFERENCE")
-
-#INCH = 152.4 # 6inch
-
-#MOTOR_X = INCH / 2 * 1.47
-#motor.location = (MOTOR_X, 0, 0)
-##motor.location = (MOTOR_X, 0, 10)
-
-## arm ----------------------------------------¬
-
-#arm = base.create_tear_beam(
-#    depth=MOTOR_X,       # ビームの長さ（Y軸）
-#    width=8,             # 断面の幅（X）
-#    height=20,           # 断面の涙型の高さ（Z）
-#    power=0.75,
-#    location=(MOTOR_X / 2, 0, 0),
-#    rotation=(0, 0, -math.pi / 2),  # Y軸→X軸方向に回転
-#)
-##arm.location = (MOTOR_X / 2, 0, 2.5)
