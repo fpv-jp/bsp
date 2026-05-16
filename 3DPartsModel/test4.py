7e5fc6644175f249f43c12d789d59b15fe7a7747
Tomokazu Tantaka <t.tantaka@eukarya.io>
2026-05-16 21:56:36 +0900

    あああ

--- 3DPartsModel/test4.py
+++ 3DPartsModel/test4.py
@@ -39,33 +39,70 @@ D = R * 10
 #   power=0.4, peak=0.25
 
 
-body = base.create_cylinder_smooth(radius=R, depth=D/3)
+body = base.create_cylinder_smooth(radius=R, depth=D/3-8)
+body_inner = base.create_cylinder_smooth(radius=R-1, depth=D/3-8)
+base.modifier_apply(body_inner, body, operation="DIFFERENCE")
+
+#base.cut_cube(
+#    target=body,
+#    scale=(R-1, R-1, D/3),
+#    location=(0, 0, 0),
+#)
 
 body1 = base.create_cylinder_smooth(radius=R, depth=D)
 base.taper(body1, segments=64, curve="tear", power=0.66)
 
+body1_inner = base.create_cylinder_smooth(radius=R-1, depth=D)
+base.taper(body1_inner, segments=64, curve="tear", power=0.66)
+base.modifier_apply(body1_inner, body1, operation="DIFFERENCE")
+
 body2 = base.create_cylinder_smooth(radius=R, depth=D)
 base.taper(body2, segments=64, curve="tear", power=0.66, peak=0.66)
+
+body2_inner = base.create_cylinder_smooth(radius=R-1, depth=D)
+base.taper(body2_inner, segments=64, curve="tear", power=0.66, peak=0.66)
+base.modifier_apply(body2_inner, body2, operation="DIFFERENCE")
 
 base.modifier_apply(obj=body1, target=body, operation="UNION")
 base.modifier_apply(obj=body2, target=body, operation="UNION")
 
+base.cut_cube(
+    target=body,
+    scale=(200, 200, 500),
+    location=(0, -100, 0),
+)
+#base.cut_cube(
+#    target=body1,
+#    scale=(200, 200, 500),
+#    location=(0, -100, 0),
+#)
+#base.cut_cube(
+#    target=body2,
+#    scale=(200, 200, 500),
+#    location=(0, -100, 0),
+#)
 
+#base.cut_cylinder(target=body, radius=R-1, depth=D/3-8)
+
+
+#body.location = (0.0, 0.0, H)
+
+#base.taper(body2, segments=64, curve="tear", power=0.66, peak=0.66)
 # 内側をくり抜く（壁厚1.5mm）
 #inner = base.create_cylinder_smooth(radius=R*0.95, depth=D)
 #base.taper(inner, segments=64, curve="tear", power=0.6, peak=0.3)
 #base.modifier_apply(inner, body, operation="DIFFERENCE")
 
 
-INCH = 12 * 25.4  # 12inch
+#INCH = 12 * 25.4  # 12inch
 
-arm_width = 14.5
-arm_height = arm_width*3
-arm_power = 0.75
+#arm_width = 14.5
+#arm_height = arm_width*3
+#arm_power = 0.75
 
-arm = base.create_tear_beam(
-    depth=INCH,
-    width=arm_width,
-    height=arm_height,
-    power=arm_power,
-)
+#arm = base.create_tear_beam(
+#    depth=INCH,
+#    width=arm_width,
+#    height=arm_height,
+#    power=arm_power,
+#)
