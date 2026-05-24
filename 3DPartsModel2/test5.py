a49103d2c5e09de57459ce8269e6273dad5eb7c3
Tomokazu Tantaka <t.tantaka@eukarya.io>
2026-05-24 13:20:48 +0900

    test

--- 3DPartsModel2/test5.py
+++ 3DPartsModel2/test5.py
@@ -13,18 +13,21 @@ import base
 
 base.init()
 
-
 adjustment = 1.47  # アームの長さ/モータ位置を調整する倍率
 
-INCH = 6 * 25.4 * adjustment  # 6.5inch
+PROP_INCH = 6 * 25.4  # プロペラ(6inch)
 
-ARM_L = INCH / 2
-ARM_W = 12.0
+DRONE_SIZE = 6 * 25.4 * adjustment  # 6inch
 
-MOTOR_PITCH = 19.0 / 2
+MOTOR_PITCH = DRONE_SIZE / 2  # モータとボディのピッチ/アームの長さ
+ARM_width = 12.0  # アームの幅
 
-MAIN_DEPTH = 6.0
+MOTOR_PITCH = 19.0 / 2  # モータの取り付け穴ピッチ
 
+ARM_THICKNESS = 6.0  # アームの太さ
+PLATE_THICKNESS = 3.0  # 天板/底板の厚み
+
+# ネジ(φ)
 M3 = 3.2
 M5 = 5.2
 
@@ -33,7 +36,6 @@ FC_PITCH = 30.5 / 2  # FC/ESCの取り付けピンのピッチ
 
 # 参考：モータ+プロペラ<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 def create_dummy_motor():
-    PROP_INCH = 6 * 25.4  # 6inch
     PROP_DEPTH = 9.0
 
     MOTOR_SIZE = 37.0
@@ -73,7 +75,7 @@ def create_dummy_motor():
     )
 
     # モータの位置調整
-    m.location = (0.0, 0.0, -(MOTOR_Z1 + MAIN_DEPTH) / 2)
+    m.location = (0.0, 0.0, -(MOTOR_Z1 + ARM_THICKNESS) / 2)
     # m.rotation_euler[0] = math.pi
     return m
 
@@ -82,16 +84,15 @@ def create_dummy_motor():
 # ボディプレート
 # --------------------------------------
 def create_body():
-    PLATE_DEPTH = 3.0
 
     # 基板プレート
     body = base.create_cylinder(
         radius=24.5,
-        depth=PLATE_DEPTH,
+        depth=PLATE_THICKNESS,
     )
 
     # 中央穴(φ5)
-    base.cut_cylinder(target=body, radius=M5 / 2, depth=PLATE_DEPTH * 2)
+    base.cut_cylinder(target=body, radius=M5 / 2, depth=PLATE_THICKNESS * 2)
 
     FC_HOLES = [
         (FC_PITCH, FC_PITCH),
@@ -106,7 +107,7 @@ def create_body():
             target=body,
             outer_radius=M3 * 2,
             inner_radius=M3 / 2,
-            depth=PLATE_DEPTH,
+            depth=PLATE_THICKNESS,
             location=(x, y, 0.0),
         )
 
@@ -116,7 +117,7 @@ def create_body():
             target=body,
             outer_radius=M3 * 2,
             inner_radius=M3 / 2,
-            depth=PLATE_DEPTH,
+            depth=PLATE_THICKNESS,
             location=(x, y, 0.0),
         )
     return body
@@ -131,12 +132,14 @@ motor = base.create_cube(
     scale=(
         MOTOR_PITCH * 2.08,
         MOTOR_PITCH * 2.08,
-        MAIN_DEPTH,
+        ARM_THICKNESS,
     )
 )
 
 # モータの回転軸と干渉する部分をカット ----------------------------
-base.cut_cylinder(target=motor, radius=M5 / 2, depth=MAIN_DEPTH * 2, location=(0.0, ARM_L, 0.0))
+base.cut_cylinder(
+    target=motor, radius=M5 / 2, depth=ARM_THICKNESS * 2, location=(0.0, MOTOR_PITCH, 0.0)
+)
 
 # 参考：dummyモータ+プロペラ<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 base.modifier_apply(obj=create_dummy_motor(), target=motor, operation="UNION")
@@ -144,8 +147,8 @@ base.modifier_apply(obj=create_dummy_motor(), target=motor, operation="UNION")
 # アーム を取り付け ----------------------------
 base.add_cube(
     target=motor,
-    scale=(ARM_W, ARM_L, MAIN_DEPTH),
-    location=(0.0, ARM_L / 2, 0.0),
+    scale=(ARM_width, MOTOR_PITCH, ARM_THICKNESS),
+    location=(0.0, MOTOR_PITCH / 2, 0.0),
 )
 
 # 45° 回転 ----------------------------
@@ -164,14 +167,14 @@ for i, (x, y) in enumerate(MOTOR_HOLES):
         target=motor,
         outer_radius=M3 * 1.5,
         inner_radius=M3 / 2,
-        depth=MAIN_DEPTH,
+        depth=ARM_THICKNESS,
         location=(x, y, 0.0),
         vertices=64,
     )
 
 # 中心を変える ----------------------------
 motor.rotation_euler[2] = 0
-motor.location = (0.0, -ARM_L, 0.0)
+motor.location = (0.0, -MOTOR_PITCH, 0.0)
 base.set_origin(motor, (0.0, 0.0, 0.0))
 
 # 45° 回転 ----------------------------
@@ -181,7 +184,7 @@ motor.rotation_euler[2] = math.pi / 4
 base.cut_cylinder(
     target=motor,
     radius=M3 / 2,
-    depth=MAIN_DEPTH * 2,
+    depth=ARM_THICKNESS * 2,
     location=(FC_PITCH, -FC_PITCH, 0.0),
 )
 
@@ -201,19 +204,18 @@ top.location = (0.0, 0.0, 3.0 + 1.5)
 # 底板を複製 ----------------------------
 bottom = base.copy(top, location=(0.0, 0.0, -3.0 - 1.5))
 
+# ------------------------------------------------------------------------------------
+# 参考(バッテリーx2、FC, ESC)
+# ------------------------------------------------------------------------------------
 
-#### ------------------------------------------------------------------------------------
-#### 参考
-#### ------------------------------------------------------------------------------------
-
-battery = base.create_cube(scale=(37, 37, 70))
-battery1 = base.copy(battery)
+battery1 = base.create_cube(scale=(37, 37, 70))
+battery2 = base.copy(battery1)
 
 fc = base.create_cube(scale=(42, 42, 10), rotation=(0, 0, math.pi / 4))
 esc = base.create_cube(scale=(56, 59, 10))
 
-battery.location = (0.0, 0.0, 120.0)
-battery1.location = (0.0, 0.0, 45.0)
+battery1.location = (0.0, 0.0, 120.0)
+battery2.location = (0.0, 0.0, 45.0)
 
 fc.location = (0.0, 0.0, -12.0)
 esc.location = (0.0, 0.0, -23.0)
