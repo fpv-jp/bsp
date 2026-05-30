1ff2382efa85b3034fd4c071121b34ef43015c4b
Tomokazu Tantaka <t.tantaka@eukarya.io>
2026-05-30 11:38:21 +0900

    test

--- 3DPartsModel2/structure_example.py
+++ 3DPartsModel2/structure_example.py
@@ -30,6 +30,9 @@ PLATE_THICKNESS = 3.0  # 天板/底板の厚み
 MOTOR_PITCH = 19.0 / 2  # モータの取り付け穴ピッチ
 FC_PITCH = 30.5 / 2  # FC/ESCの取り付けピンのピッチ
 
+EXAMPLE = True
+EXAMPLE = False
+
 
 # 参考：モータ+プロペラ<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 def create_dummy_motor():
@@ -75,6 +78,73 @@ def create_dummy_motor():
     m.location = (0.0, 0.0, -(MOTOR_Z1 + ARM_THICKNESS) / 2)
     # m.rotation_euler[0] = math.pi
     return m
+
+
+# --------------------------------------
+# アーム
+# --------------------------------------
+def create_arm_motor():
+
+    # モータ台
+    motor = base.create_cube(
+        scale=(
+            MOTOR_PITCH * 2.08,
+            MOTOR_PITCH * 2.08,
+            ARM_THICKNESS,
+        )
+    )
+
+    ## 参考：dummyモータ+プロペラ<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    if EXAMPLE:
+        base.modifier_apply(obj=create_dummy_motor(), target=motor, operation="UNION")
+
+    # アーム を取り付け ----------------------------
+    base.add_cube(
+        target=motor,
+        scale=(ARM_width, ARM_length, ARM_THICKNESS),
+        location=(0.0, ARM_length / 2, 0.0),
+    )
+    # モータの回転軸と干渉する部分をカット ----------------------------
+    base.cut_cylinder(
+        target=motor, radius=M5 / 2, depth=ARM_THICKNESS * 2, location=(0.0, ARM_length, 0.0)
+    )
+
+    motor.rotation_euler[2] = math.pi / 4
+
+    # アーム と モータ の取り付け穴 ----------------------------
+    MOTOR_HOLES = [
+        (MOTOR_PITCH, 0),
+        (-MOTOR_PITCH, 0),
+        (0, MOTOR_PITCH),
+        (0, -MOTOR_PITCH),
+    ]
+    for i, (x, y) in enumerate(MOTOR_HOLES):
+        base.add_ring(
+            target=motor,
+            outer_radius=M3 * 1.5,
+            inner_radius=M3 / 2,
+            depth=ARM_THICKNESS,
+            location=(x, y, 0.0),
+            vertices=64,
+        )
+
+    # 中心を変える ----------------------------
+    motor.rotation_euler[2] = 0
+    motor.location = (0.0, -ARM_length, 0.0)
+    base.set_origin(motor, (0.0, 0.0, 0.0))
+
+    # 45° 回転 ----------------------------
+    motor.rotation_euler[2] = math.pi / 4
+
+    # アーム と ボディプレート の取り付け穴 ----------------------------
+    base.cut_cylinder(
+        target=motor, radius=M3 / 2, depth=ARM_THICKNESS * 2, location=(FC_PITCH, -FC_PITCH, 0.0)
+    )
+
+    # アーム 同士が干渉する角をカット ----------------------------
+    base.cut_cube(target=motor, scale=(20, 10, 10), location=(0.0, 5.0, 0.0))
+    base.cut_cube(target=motor, scale=(10, 20, 10), location=(-5.0, 0.0, 0.0))
+    return motor
 
 
 # --------------------------------------
@@ -124,70 +194,17 @@ def create_plate():
 # 組み立て
 # ------------------------------------------------------------------------------------
 
-# モータ台
-motor = base.create_cube(
-    scale=(
-        MOTOR_PITCH * 2.08,
-        MOTOR_PITCH * 2.08,
-        ARM_THICKNESS,
-    )
-)
+arm_motor = create_arm_motor()
 
-# 参考：dummyモータ+プロペラ<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-base.modifier_apply(obj=create_dummy_motor(), target=motor, operation="UNION")
-
-# アーム を取り付け ----------------------------
-base.add_cube(
-    target=motor,
-    scale=(ARM_width, ARM_length, ARM_THICKNESS),
-    location=(0.0, ARM_length / 2, 0.0),
-)
-# モータの回転軸と干渉する部分をカット ----------------------------
-base.cut_cylinder(
-    target=motor, radius=M5 / 2, depth=ARM_THICKNESS * 2, location=(0.0, ARM_length, 0.0)
-)
-
-motor.rotation_euler[2] = math.pi / 4
-
-
-# アーム と モータ の取り付け穴 ----------------------------
-MOTOR_HOLES = [
-    (MOTOR_PITCH, 0),
-    (-MOTOR_PITCH, 0),
-    (0, MOTOR_PITCH),
-    (0, -MOTOR_PITCH),
-]
-for i, (x, y) in enumerate(MOTOR_HOLES):
-    base.add_ring(
-        target=motor,
-        outer_radius=M3 * 1.5,
-        inner_radius=M3 / 2,
-        depth=ARM_THICKNESS,
-        location=(x, y, 0.0),
-        vertices=64,
-    )
-
-# 中心を変える ----------------------------
-motor.rotation_euler[2] = 0
-motor.location = (0.0, -ARM_length, 0.0)
-base.set_origin(motor, (0.0, 0.0, 0.0))
-
-# 45° 回転 ----------------------------
-motor.rotation_euler[2] = math.pi / 4
-
-# アーム と ボディプレート の取り付け穴 ----------------------------
-base.cut_cylinder(
-    target=motor, radius=M3 / 2, depth=ARM_THICKNESS * 2, location=(FC_PITCH, -FC_PITCH, 0.0)
-)
-
-# アーム 同士が干渉する角をカット ----------------------------
-base.cut_cube(target=motor, scale=(20, 10, 10), location=(0.0, 5.0, 0.0))
-base.cut_cube(target=motor, scale=(10, 20, 10), location=(-5.0, 0.0, 0.0))
+# アームの出力用
+#arm_motor.rotation_euler[2] = 0
+#arm_motor.location = (0.0, ARM_length / 2, 0.0)
+#base.set_origin(arm_motor, (0.0, 0.0, 0.0))
 
 # 他の アーム を複製 ----------------------------
 for i, (x) in enumerate([math.pi / 2, math.pi, -math.pi / 2]):
-    base.copy(motor, rotation=(0, 0, x))
-motor.rotation_euler[2] = 0
+    base.copy(arm_motor, rotation=(0, 0, x))
+arm_motor.rotation_euler[2] = 0
 
 # 天板 ----------------------------
 top = create_plate()
@@ -199,22 +216,22 @@ bottom = base.copy(top, location=(0.0, 0.0, -3.0 - 1.5))
 # ------------------------------------------------------------------------------------
 # 参考(バッテリーx2、FC, ESC)
 # ------------------------------------------------------------------------------------
+if EXAMPLE:
+    battery1 = base.create_cube(scale=(37, 37, 70))
+    battery2 = base.copy(battery1)
 
-battery1 = base.create_cube(scale=(37, 37, 70))
-battery2 = base.copy(battery1)
+    fc = base.create_cube(scale=(40, 40, 10), rotation=(0, 0, math.pi / 4))
+    # esc = base.create_cube(scale=(56, 59, 10))
+    esc = base.create_cube(scale=(40, 40, 10))
 
-fc = base.create_cube(scale=(40, 40, 10), rotation=(0, 0, math.pi / 4))
-# esc = base.create_cube(scale=(56, 59, 10))
-esc = base.create_cube(scale=(40, 40, 10))
+    battery1.location = (0.0, 0.0, 128.0)
+    battery2.location = (0.0, 0.0, 54.0)
 
-battery1.location = (0.0, 0.0, 128.0)
-battery2.location = (0.0, 0.0, 54.0)
+    fc.location = (0.0, 0.0, 12.0)
+    esc.location = (0.0, 0.0, -12.0)
 
-fc.location = (0.0, 0.0, 12.0)
-esc.location = (0.0, 0.0, -12.0)
-
-bpy.ops.object.select_all(action="SELECT")
-bpy.context.view_layer.objects.active = next(iter(bpy.context.scene.objects))
-bpy.ops.object.join()
-bpy.context.active_object.rotation_euler[2] = math.pi / 4
-bpy.context.active_object.location.z -= 76
+    bpy.ops.object.select_all(action="SELECT")
+    bpy.context.view_layer.objects.active = next(iter(bpy.context.scene.objects))
+    bpy.ops.object.join()
+    bpy.context.active_object.rotation_euler[2] = math.pi / 4
+    bpy.context.active_object.location.z -= 76
